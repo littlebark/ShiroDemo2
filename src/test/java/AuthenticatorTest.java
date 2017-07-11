38f9b4881da7278b4dd7ba892f11fc997ceba5ca
littlebark <121792968@qq.com>
2017-07-11 17:34:01 +0800

    Initial commit

---
+++ src/test/java/AuthenticatorTest.java
@@ -0,0 +1,86 @@
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by littlebark on 2017/7/11.
+ */
+
+//ModularRealmAuthenticator默认使用AtLeastOneSuccessfulStrategy策略。
+//如果需要改变认证策略，则需要改对应的配置文件
+//设置到securityManager.authenticator.authenticationStrategy这个里面即可
+public class AuthenticatorTest {
+    private void login(String configFile) {
+        //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+        Factory<SecurityManager> factory =
+                new IniSecurityManagerFactory(configFile);
+
+        //2、得到SecurityManager实例 并绑定给SecurityUtils
+        org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        //3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+
+        subject.login(token);
+    }
+    @Test
+    public void testAllSuccessfulStrategyWithSuccess() {
+        login("classpath:shiro-authenticator-all-success.ini");
+        Subject subject = SecurityUtils.getSubject();
+
+        //得到一个身份集合，其包含了Realm验证成功的身份信息
+        PrincipalCollection principalCollection = subject.getPrincipals();
+        Assert.assertEquals(2, principalCollection.asList().size());
+    }
+    //抛出异常则代表测试通过
+    @Test(expected = UnknownAccountException.class)
+    public void testAllSuccessfulStrategyWithFail() {
+        login("classpath:shiro-authenticator-all-fail.ini");
+        Subject subject = SecurityUtils.getSubject();
+    }
+
+    @Test
+    public void testAtLeastOneSuccessfulStrategyWithSuccess() {
+        login("classpath:shiro-authenticator-atLeastOne-success.ini");
+        Subject subject = SecurityUtils.getSubject();
+
+        //得到一个身份集合，其包含了Realm验证成功的身份信息
+        PrincipalCollection principalCollection = subject.getPrincipals();
+        Assert.assertEquals(2, principalCollection.asList().size());
+    }
+    @Test
+    public void testAtLeastTwoSuccessfulStrategyWithSuccess() {
+        login("classpath:shiro-authenticator-atLeastTwo-success.ini");
+        Subject subject = SecurityUtils.getSubject();
+
+        //得到一个身份集合，其包含了Realm验证成功的身份信息
+        PrincipalCollection principalCollection = subject.getPrincipals();
+        Assert.assertEquals(2, principalCollection.asList().size());
+    }
+    @Test(expected = AuthenticationException.class)
+    public void testOnlyOneSuccessfulStrategyWithSuccess() {
+        login("classpath:shiro-authenticator-onlyOne-success.ini");
+        Subject subject = SecurityUtils.getSubject();
+
+        //肯定是失败的，因为有2个通过,会打印出myrealm1和myrealm3
+    }
+    @Test
+    public void testFirstOneSuccessfulStrategyWithSuccess() {
+        login("classpath:shiro-authenticator-first-success.ini");
+        Subject subject = SecurityUtils.getSubject();
+
+        //得到一个身份集合，其包含了第一个Realm验证成功的身份信息
+        PrincipalCollection principalCollection = subject.getPrincipals();
+        Assert.assertEquals(1, principalCollection.asList().size());
+    }
+}
---
+++ src/test/java/com/shirodemo/realm/MyRealm1.java
@@ -0,0 +1,33 @@
+package com.shirodemo.realm;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * Created by littlebark on 2017/7/11.
+ */
+public class MyRealm1 implements Realm{
+    @Override
+    public String getName() {
+        return "myrealm1";
+    }
+
+    @Override
+    public boolean supports(AuthenticationToken authenticationToken) {
+        return authenticationToken instanceof UsernamePasswordToken;
+    }
+
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        String username = (String)token.getPrincipal();  //得到用户名
+        String password = new String((char[])token.getCredentials()); //得到密码
+        if(!"zhang".equals(username)) {
+            throw new UnknownAccountException(); //如果用户名错误
+        }
+        if(!"123".equals(password)) {
+            throw new IncorrectCredentialsException(); //如果密码错误
+        }
+        //如果身份认证验证成功，返回一个AuthenticationInfo实现；
+        return new SimpleAuthenticationInfo(username, password, getName());
+    }
+}
